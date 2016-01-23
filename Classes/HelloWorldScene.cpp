eba2913a10cdcf46de810ee3efb829b70fc99448
erfanumar <erfanspecialist@gmail.com>
2016-01-23 12:20:30 +0800

    commit message

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,48 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+
+	auto mySprite = Sprite::create("girl.png");
+	mySprite->setPosition(Vec2(100,100));
+	this->addChild(mySprite);
+
+	auto move = MoveTo::create(3, Vec2(200,100));
+	mySprite->runAction(move);
+
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
